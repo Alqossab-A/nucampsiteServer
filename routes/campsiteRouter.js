1e91055e9fd6fa6a3d9d334fde6f68d6b2088cff
Ahmed Al-Qossab <alqossaba@gmail.com>
2022-08-07 13:54:44 -0400

    mongoose populate

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -8,6 +8,7 @@ campsiteRouter
     .route('/')
     .get((req, res, next) => {
         Campsite.find()
+        .populate('comments.author')
             .then((campsites) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -46,6 +47,7 @@ campsiteRouter
     .route('/:campsiteId')
     .get((req, res, next) => {
         Campsite.findById(req.params.campsiteId)
+        .populate('comments.author')
             .then((campsite) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -91,6 +93,7 @@ campsiteRouter
     .route('/:campsiteId/comments')
     .get((req, res, next) => {
         Campsite.findById(req.params.campsiteId)
+        .populate('comments.author')
             .then((campsite) => {
                 if (campsite) {
                     res.statusCode = 200;
@@ -111,6 +114,7 @@ campsiteRouter
         Campsite.findById(req.params.campsiteId)
             .then((campsite) => {
                 if (campsite) {
+                    req.body.author = req.user._id;
                     campsite.comments.push(req.body);
                     campsite
                         .save()
@@ -168,6 +172,7 @@ campsiteRouter
     .route('/:campsiteId/comments/:commentId')
     .get((req, res, next) => {
         Campsite.findById(req.params.campsiteId)
+        .populate('comments.author')
             .then((campsite) => {
                 if (campsite && campsite.comments.id(req.params.commentId)) {
                     res.statusCode = 200;
